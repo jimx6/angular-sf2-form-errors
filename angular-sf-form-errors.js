0d7297d92c207cb16c81e6251bb374a08adf0172
michal <m.jankowski@miart.pl>
2014-09-24 14:54:22 +0200

    init commit

---
+++ angular-sf-form-errors.js
@@ -0,0 +1,18 @@
+/**
+ * @license mijankowski
+ * License: MIT
+ */
+(function(window, angular) {'use strict';
+
+    angular.module('sfFormErrors', []).
+        factory('$sfFormErrors', [function () {
+            return {
+
+                render: function(errors) {
+
+                }
+            };
+
+        }]);
+
+})(window, window.angular);
